9b4e4ce62f9f28bd1b170bb5bbd921b9129e12d2
Rafael Marinho de Araújo <rma6@cin.ufpe.br>
2018-05-12 22:29:00 -0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,9 +1,39 @@
+lock=0;
+
+async function enqueue(result)
+{
+  if(lock==0)
+  {
+    lock=1;
+    console.log('calling norenew for '+result[joins].CPF);
+    const child = spawn('node', ['/home/rafaelmarinhoa/RALB/norenew.js', result[joins].CPF, result[joins].Password]);
+    child.stdout.on('data', (data) => {
+      var msg = data.toString();
+      msg=msg.substring(0, msg.length-1);
+      console.log(msg);
+    });
+    child.on('exit', function (code) {
+      joins++;
+      lock=0;
+    });
+  }
+  if(result.length==joins)
+  {
+    clearInterval(timer);
+    console.log('fetch done');
+    process.exit();
+  }
+}
+
 var mysql = require('mysql');
+var { spawn } = require('child_process');
+
+joins=0;
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "web",
-  password: "",
+  password: "qsXvEsmoT9TcVRpmBw5WZ6vIJmEZoF0j",
   database: "RALB"
 });
 console.log(Date());
@@ -17,11 +47,6 @@ con.connect(function(err)
   {
     if (err) throw err;
     console.log('query OK');
-    const { execFileSync } = require('child_process');//Sync
-    var i;
-    for (i = 0; i < result.length; i++)
-    {
-      execFileSync('node', ['/home/rafaelmarinhoa/RALB/norenew.js', result[i].CPF, result[i].Password]);
-    }
+    timer = setInterval(enqueue, 1000, result);
   });
 });
